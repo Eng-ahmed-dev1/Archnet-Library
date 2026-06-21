c4d555811aae410c4c94f21beac5efbe36dfd19d
Ahmed <ahmed.devmail1@gmail.com>
2026-06-21 22:42:29 +0300

    Refactor command initialization and usage message handling

--- Archnet.Cli/Program.cs
+++ Archnet.Cli/Program.cs
@@ -1,18 +1,21 @@
-﻿using Archnet.Cli.Commands;
+﻿using System.Reflection;
+using Archnet.Cli.Commands;
 using Archnet.Cli.Services;
 
-var dispatcher =
-    new CommandDispatcher(
-    [
-        new NewCommand()
-    ]);
+var commands = Assembly.GetExecutingAssembly()
+                .GetTypes()
+                .Where(t => typeof(IArchCommand).IsAssignableFrom(t)
+                && t is { IsClass: true, IsAbstract: false }
+                ).Select(t => (IArchCommand)Activator.CreateInstance(t)!)
+                .ToList();
 
+var dispatcher = new CommandDispatcher(commands);
 if (args.Length == 0)
 {
-    Console.WriteLine(
-        "Usage: archnet <command>");
-
+    Console.WriteLine("Usage : archnet <command>");
     return;
 }
+var command = args[0];
+var commandArgs = args.Skip(1).ToArray();
 
-await dispatcher.DispatchAsync(args[0]);
+await dispatcher.DispatchAsync(command, commandArgs);
