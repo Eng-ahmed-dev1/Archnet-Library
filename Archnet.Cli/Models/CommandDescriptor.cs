7e93f7bbc54557eed0a7aea4c440fa62e967489d
Ahmed <ahmed.devmail1@gmail.com>
2026-06-21 23:29:40 +0300

    Add CommandDescriptor class to encapsulate command name and command implementation

---
+++ Archnet.Cli/Models/CommandDescriptor.cs
@@ -0,0 +1,10 @@
+using Archnet.Cli.Commands;
+
+namespace Archnet.Cli.Models;
+
+public class CommandDescriptor
+{
+    public string Name { get; set; } = string.Empty;
+
+    public IArchCommand Command { get; set; } = default!;
+}
