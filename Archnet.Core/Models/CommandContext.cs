1dd02e8f6709f28a1fc17ed2aa74203cb95371fc
Ahmed <ahmed.devmail1@gmail.com>
2026-06-21 23:30:08 +0300

    Add CommandContext class to encapsulate command details and options

---
+++ Archnet.Core/Models/CommandContext.cs
@@ -0,0 +1,6 @@
+public class CommandContext
+{
+    public string Command { get; set; } = "";
+    public string ProjectName { get; set; } = "";
+    public Dictionary<string, string> Options { get; set; } = new();
+}
