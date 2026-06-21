909b26469173eccafc6ff32ee7beb92f4d0d9db6
Ahmed <ahmed.devmail1@gmail.com>
2026-06-21 23:06:15 +0300

    Refactor command initialization to include command attributes and improve command creation logic

--- Archnet.Cli/Program.cs
+++ Archnet.Cli/Program.cs
@@ -1,13 +1,30 @@
 ﻿using System.Reflection;
+using Archnet.Cli.Attributes;
 using Archnet.Cli.Commands;
 using Archnet.Cli.Services;
 
-var commands = Assembly.GetExecutingAssembly()
-                .GetTypes()
-                .Where(t => typeof(IArchCommand).IsAssignableFrom(t)
-                && t is { IsClass: true, IsAbstract: false }
-                ).Select(t => (IArchCommand)Activator.CreateInstance(t)!)
-                .ToList();
+var commands =
+    Assembly.GetExecutingAssembly()
+        .GetTypes()
+        .Where(t => typeof(IArchCommand).IsAssignableFrom(t)
+                    && t is { IsClass: true, IsAbstract: false })
+        .Select(t =>
+        {
+            var attr = t.GetCustomAttribute<CommandAttribute>();
+
+            if (attr is null)
+                return null;
+
+            var instance = (IArchCommand)Activator.CreateInstance(t)!;
+
+            return new
+            {
+                Name = attr.Name,
+                Command = instance
+            };
+        })
+        .Where(x => x != null)
+        .ToList()!;
 
 var dispatcher = new CommandDispatcher(commands);
 if (args.Length == 0)
